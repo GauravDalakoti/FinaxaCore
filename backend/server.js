f0c7ef002ecfcf3363db9b4c284aea63549aa56e
gaurav dalakoti <gauravdalakoti046@gmail.com>
2026-04-04 15:01:41 +0530

    fixes in server.js file

--- backend/server.js
+++ backend/server.js
@@ -50,9 +50,9 @@ app.use('*', (req, res) => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(` Server running on port ${PORT} in ${process.env.NODE_ENV || 'development'} mode`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(` Server running on port ${PORT} in ${process.env.NODE_ENV || 'development'} mode`);
+// });
 
 export default app;
